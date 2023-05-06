f18c47253770aee78d662327767b75e6a2173fe1
Hyan Ferreira <114369661+HyanFerreira@users.noreply.github.com>
2023-05-05 22:48:58 -0300

    Atualização 05/05

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelector(el);
+
+//Listagem de modelos
+modeloJson.map((item, index) => {
+  let modeloItem = c(".models .modelo-item").cloneNode(true);
+
+  modeloItem.querySelector(".modelo-item-img img").src = item.img;
+  modeloItem.querySelector(".modelo-item-name").innerHTML = item.name;
+  modeloItem.querySelector(".modelo-item-desc").innerHTML = item.description;
+  modeloItem.querySelector(".modelo-item-price").innerHTML = `R$ ${item.price.toFixed(2)}`;
+
+  modeloItem.querySelector("a").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    c(".modeloModal").style.display = "Flex";
+  });
+  
+  c(".modelo-area").append(modeloItem);
+});
