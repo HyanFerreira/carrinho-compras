4e821cbc3c2e54a14e68c9f7eebaf9d717da1746
Hyan Ferreira <114369661+HyanFerreira@users.noreply.github.com>
2023-05-06 11:03:15 -0300

    Atualização 06/05

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 const c = (el) => document.querySelector(el);
-const cs = (el) => document.querySelectorAll(el);
+const cs = (el) => document.querySelector(el);
 
-// Listagem dos modelos
+//Listagem de modelos
 modeloJson.map((item, index) => {
-  let modeloItem = c('.models .modelo-item').cloneNode(true);
+  let modeloItem = c(".models .modelo-item").cloneNode(true);
 
-  modeloItem.querySelector('.modelo-item-img img').src = item.img;
-  modeloItem.querySelector('.modelo-item-name').innerHTML = item.name;
-  modeloItem.querySelector('.modelo-item-desc').innerHTML = item.description;
-  modeloItem.querySelector('.modelo-item-price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+  modeloItem.querySelector(".modelo-item-img img").src = item.img;
+  modeloItem.querySelector(".modelo-item-name").innerHTML = item.name;
+  modeloItem.querySelector(".modelo-item-desc").innerHTML = item.description;
+  modeloItem.querySelector(".modelo-item-price").innerHTML = `R$ ${item.price.toFixed(2)}`;
 
-  c('.modelo-area').append(modeloItem);
+  modeloItem.querySelector("a").addEventListener("click", (e) => {
+    e.preventDefault();
+
+    c(".modeloModal").style.display = "Flex";
+  });
+  
+  c(".modelo-area").append(modeloItem);
 });
